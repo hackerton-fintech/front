653ae02f3b293a7d780cab15eaaa92ed5431918e
Garo-o <hjs8482@naver.com>
2022-06-24 16:04:45 +0900

    main-page-ing

--- fintech/src/App.vue
+++ fintech/src/App.vue
@@ -1,20 +1,31 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <router-link class="app-home-btn" to="/">Home</router-link>
     <router-view/>
   </div>
 </template>
 
+<script>
+//import HomeView from '@/views/HomeView.vue'
+
+export default{
+  components:{
+    //HomeView,
+  },
+}
+</script>
+
 <style>
+.app-home-btn{
+  position: fixed;
+  top:30px;
+  right: 20px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 nav {
--- fintech/src/views/HomeView.vue
+++ fintech/src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="display:flex;">
+    <div>
+      
+    </div>
+    <div class="recommand-btn">
+      <div>
+        <router-link to="">주식</router-link>
+      </div>
+      <div>
+        <router-link to="">채권</router-link>
+      </div>
+      <div>
+        <router-link to="">예금</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+
   }
 }
 </script>
+
+<style>
+  .recommand-btn *{
+    text-decoration: none;
+    color: black;
+  }
+</style>
