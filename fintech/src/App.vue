36057059e792da6965009c92e1cef89c4b2bce00
Garo-o <hjs8482@naver.com>
2022-06-25 22:18:22 +0900

    main page -ing

--- fintech/src/App.vue
+++ fintech/src/App.vue
@@ -1,35 +1,86 @@
 <template>
-  <div id="app" style="background-color: gold; height: 844px;">
-    <div class="app-home-btn">
-      <router-link :to="{name: 'login'}" v-if="!isLogin">Login</router-link>
-      <button v-else @click="logOut" style="border:none; background-color: gold; font-size: 1rem; outline:0;">LogOut</button>
-      <router-link :to="{name: join}">Join</router-link>
-    </div>
-    <router-view />
+  <div id="app" style=" height: inherit;">
+    <header style="height:20%; background-color: rgb(41,137,134);">
+      <span style="color:rgba(255,255,255,0.8); font-size:1.5rem; font:bold;">CashCow</span>
+    </header>
+    <section style="height:80%;">
+      <HomeView :user="user"></HomeView>
+    </section>
+    <!-- <div class="app-home-btn">
+      <router-link :to="{ name: 'login' }" v-if="!isLogin">Login</router-link>
+      <button v-else @click="logOut"
+        style="border:none; background-color: gold; font-size: 1rem; outline:0;">LogOut</button>
+      <router-link to="/">Join</router-link>
+    </div> -->
   </div>
 </template>
 
 <script>
-// import HomeView from '@/views/HomeView.vue'
-
+import HomeView from './views/HomeView.vue';
+// import {Plotly} from 'plotly';
 export default {
   name: "AppVue",
   data() {
     return {
-      isLogin: false
+      user:{
+        name: "펩시맨",
+        characteristic: "공격투자성향",
+      }
     }
+  },
+  watch: {
+
   },
   methods: {
     logOut() {
       window.alert("로그아웃 되었습니다.")
       this.isLogin = false;
+    },
+    login(val) {
+      console.log('parent login');
+      this.isLogin = val;
+      this.$router.push("/");
     }
   },
   components: {
-  },
+    HomeView
+},
+  created() {
+  }
 };
+
 </script>
 
+
+<style scoped>
+.v-application * *{
+  font-family: 'GmarketSans', sans-serif !important;
+}
+@font-face {
+    font-family: 'GmarketSans';
+    font-weight: 700; 
+    font-style: normal; 
+    src: url(https://cdn.jsdelivr.net/gh/webfontworld/GmarketSans/GmarketSansTTFBold.woff2) format('woff2'),
+         url(https://cdn.jsdelivr.net/gh/webfontworld/GmarketSans/GmarketSansTTFBold.woff) format('woff');
+    font-display: swap;
+}
+@font-face {
+    font-family: 'GmarketSans';
+    font-weight: 500; 
+    font-style: normal; 
+    src: url(https://cdn.jsdelivr.net/gh/webfontworld/GmarketSans/GmarketSansTTFMedium.woff2) format('woff2'),
+         url(https://cdn.jsdelivr.net/gh/webfontworld/GmarketSans/GmarketSansTTFMedium.woff) format('woff');
+    font-display: swap;
+}
+@font-face {
+    font-family: 'GmarketSans';
+    font-weight: 300; 
+    font-style: normal; 
+    src: url(https://cdn.jsdelivr.net/gh/webfontworld/GmarketSans/GmarketSansTTFLight.woff2) format('woff2'),
+         url(https://cdn.jsdelivr.net/gh/webfontworld/GmarketSans/GmarketSansTTFLight.woff) format('woff');
+    font-display: swap;
+}
+</style>
 <style>
 .app-home-btn a {
   text-decoration: none;
@@ -62,4 +113,5 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
 </style>
--- fintech/src/views/HomeView.vue
+++ fintech/src/views/HomeView.vue
@@ -1,33 +1,41 @@
 <template>
   <div style="display:flex; height:100%; flex-direction: column;">
-    <div
-      style="display:flex; flex-direction:row;  box-shadow: 0.3em 0.3em 1em rgba(0,0,0,0.5); background-color: white;  border-radius:8px; width:80%; margin:0 auto; margin-top: 100px;">
-      
-        <img src="@/assets/93493545-people-icon.jpg" style="width:30%;margin:5px 5px 5px 5px;" />
-      
+    <div style="display:flex; flex-direction:row; background-color: rgb(41,137,134); height:50%">
 
-      <div style="display:flex; flex-direction: column;justify-content: center; margin-left: 20px;">
-        <div style="font-size:20px;">
-          투자 성향
-        </div>
 
-        <div style="text-align: center;">사용자</div>
 
-        <div>기타</div>
-        <animated-number :value="100000000" :formatValue="formatMoney" :duration="1500" />
+      <div class="info-user-detail"
+        style="display:flex; flex-direction: column; flex-grow: 1; justify-content: start; align-items: flex-start;  margin-left: 20px; height: inherit; padding-bottom: 10%; padding-top:15%;">
+
+        <animated-number :value="1000000" :formatValue="formatMoney" :duration="1500"
+          style="margin:0 auto; color:white; font-weight:900; font-size:2rem;" />
+
+        <span style="font-size:150%; font-weight: 700; padding-top: 5%;">
+          {{ user.characteristic }}
+        </span>
+
+        <span>{{ user.name }}</span>
+
+
       </div>
     </div>
 
-    <div class="recommand-btn"
-      style="width:80%; box-shadow: 0.3em 0.3em 1em rgba(0,0,0,0.5); margin: 0 auto; margin-top: 30px; margin-bottom: 30px; background-color: white; border-radius: 10px;">
+    <div class="recommand-btn" style="width:100%;margin-top: 5%; margin-bottom: 5%; background-color: white;">
       <div v-for="item in items" :key="item.id">
 
-        <div style="margin-top:5px">
-          <router-link :to="{ name: item.link }">
+        <div>
+          <router-link :to="{ name: item.link }"
+            style="display:flex; flex-direction: row; align-items: center; justify-content: space-between;">
             <div class="bar-graph">
 
               <div class="content" :data-width="`${item.percent}`" style=";">
-                <animated-number :value="`${item.percent}`" :formatValue="format" :duration="1500" />
+              </div>
+              <animated-number class="contnet-num" :value="`${item.percent}`" :formatValue="format" :duration="1500"
+                style="color:rgba(41,137,134,1); margin: 3px 5px 1px 1px; font-size:1.3rem;" />
+            </div>
+            <div style="display:flex; justify-content: center; flex-grow: 1;">
+              <div>
+                <span style="color:rgba(41, 137, 134, 0.9); font-size:1rem; font-weight: 600;">{{ item.name }}</span>
               </div>
             </div>
           </router-link>
@@ -38,14 +46,22 @@
 </template>
 
 <script>
-import axios from 'axios';
 import AnimatedNumber from "animated-number-vue";
+
 export default {
   name: 'HomeView',
   data() {
     return {
       items: [],
     }
+  },
+  props: {
+    user: {
+      name: String,
+      characteristic: String,
+
+    },
+
   },
   components: {
     AnimatedNumber
@@ -54,25 +70,20 @@ export default {
   },
   methods: {
     formatMoney(value) {
-      return `￦ ${value.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+      return `￦${value.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
     },
     format(value) {
-      return `${value.toFixed(0)}`;
+      return `${value.toFixed(2)}`;
     }
   },
   created() {
-    axios.get('http://localhost:8888/info')
-      .then((res) => {
-        this.items = res.data;
-      })
-    this.items = [
-      { link: "stock", name: "주식", percent: "14" },
-      { link: "bond", name: "채권", percent: "60" },
-      { link: "deposit", name: "예금", percent: "15" },
-      { link: "DepositView", name: "123", percent: "5" },
-      { link: "DepositView", name: "asd", percent: "6" },
-    ];
-    console.log(this.items)
+    const name  = ['예금 비중',	'채권 비중',	'주식 비중',	'금 비중',	'원자재 비중',	'부동산 비중',	'나스닥 비중']
+    const link = ['deposit', 'bond', 'stock', 'gold', 'material', 'house', 'nasdok']
+    const val = [0.15511477,	0.18381329,	0.08386336,	0.22952831,	0.08252258,	0.11588029,	0.14927739]
+
+    for(let i =0; i<name.length; i++){
+      this.items.push({link:link[i], name:(name[i].split(" "))[0], percent:(val[i]*100), image:""});
+    }
   },
   mounted() {
     document.querySelectorAll(".bar-graph div").forEach(el => {
@@ -85,6 +96,11 @@ export default {
 </script>
 
 <style>
+.info-user-detail span {
+  font-size: 1rem;
+  color: rgba(255, 255, 255, 0.9);
+}
+
 .tran-enter-to {
   transition: all 1.5s ease;
 }
@@ -95,12 +111,16 @@ export default {
 }
 
 .bar-graph {
-  color: #fff;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  margin-top: 10px;
   font-size: 11px;
   line-height: 25px;
-  height: 25px;
-  margin-bottom: 5px;
-  background-color: white;
+  height: 20px;
+  width: 80%;
+  background-color: rgba(41, 137, 134, 0.1);
   -webkit-border-radius: 10px;
   -moz-border-radius: 10px;
   -ms-border-radius: 10px;
@@ -115,16 +135,11 @@ export default {
   transition: all ease 1.5s;
 }
 
-.bar-graph div {
-  box-shadow: 0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
-  background-color: #ffc600;
+.bar-graph .content {
+  background-color: rgba(41, 137, 134, 0.4);
   position: relative;
   text-align: center;
-  margin-left: 5px;
   padding-left: 25px;
-  -webkit-border-radius: 10px;
-  -moz-border-radius: 10px;
-  -ms-border-radius: 10px;
-  border-radius: 10px;
+  border-radius: 5px;
 }
 </style>
